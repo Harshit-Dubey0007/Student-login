3d607dd195287f157566696538808464de32dc9e
Harshit-Dubey0007 <hdubey399@gmail.com>
2026-07-08 23:15:12 +0530

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -1,4 +1,7 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCaSum-MRp6eWVxaK2sGGRZBVAIIDesTA0",
   authDomain: "student-login-49bc8.firebaseapp.com",
@@ -8,3 +11,9 @@ const firebaseConfig = {
   appId: "1:411454234875:web:b402a24f86a8dd1d5c505e",
   measurementId: "G-W400983Q16"
 };
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
