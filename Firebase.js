6721bd07ee15b14b2545f6e00a4ddbe395fb8848
Harshit-Dubey0007 <hdubey399@gmail.com>
2026-07-08 23:08:13 +0530

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -1,9 +1,10 @@
-rules_version = '2';
-
-service cloud.firestore {
-  match /databases/{database}/documents {
-    match /{document=**} {
-      allow read, write: if false;
-    }
-  }
-      }
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCaSum-MRp6eWVxaK2sGGRZBVAIIDesTA0",
+  authDomain: "student-login-49bc8.firebaseapp.com",
+  projectId: "student-login-49bc8",
+  storageBucket: "student-login-49bc8.firebasestorage.app",
+  messagingSenderId: "411454234875",
+  appId: "1:411454234875:web:b402a24f86a8dd1d5c505e",
+  measurementId: "G-W400983Q16"
+};
